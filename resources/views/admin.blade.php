83d19b5ae1fb756db71ae24333926330d954d209
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-11-16 18:33:39 +0800

    display all users on admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-8 mb-1">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
@@ -19,6 +19,39 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+        </div>
+
+        <div class="col-md-10 mt-1">
+            <div class="card">
+                <div class="card-header">
+                    Lists of Users
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-sm table-bordered table-hover">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Position</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>    
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->position }}</td>
+                                <td>
+                                    <button class="btn btn-sm btn-danger">Delete</button>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>    
+                </div>
+            </div>
+            
         </div>
     </div>
 </div>
