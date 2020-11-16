77517a98ebb6ebad363745fd1e86e3eb3a639610
VictorDoom022 <40799001+VictorDoom022@users.noreply.github.com>
2020-11-16 20:29:57 +0800

    change redirect path after register

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const CUSTOMER = '/customer';
+    public const INDEX = '/';
 
     /**
      * The controller namespace for the application.
